2f4d6293cdb232a2be0290f618a3db64c3ddead7
Angel39706 <96604953+Angel39706@users.noreply.github.com>
2023-12-03 15:40:23 -0800

    Initial Commit

---
+++ PartyExplorer/PartyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyExplorerApp.swift
+//  PartyExplorer
+//
+//  Created by Angel Martinez on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
